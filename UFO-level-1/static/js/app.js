493439225d96a374308e2af78ed646d5abd62b2a
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-05-05 09:16:26 -0400

    filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,11 @@
 // from data.js
 var tableData = data;
+// datetime: "1/1/2010",
+//     city: "benton",
+//     state: "ar",
+//     country: "us",
+//     shape: "circle",
+//     durationMinutes: "5 mins.",
+//     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
 
 // YOUR CODE HERE!
